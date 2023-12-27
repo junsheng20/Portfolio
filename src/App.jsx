6a59506c4cf6335ff9ddeb49a76cb518fce7c650
junsheng <leejunsheng7@gmail.com>
2023-12-27 21:02:52 +0800

    completed contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Qualification from "./components/qualification/Qualification";
@@ -14,6 +15,7 @@ export default function App() {
         <About />
         <Skills />
         <Qualification />
+        <Contact />
       </main>
     </>
   );
