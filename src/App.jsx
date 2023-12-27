3af715864b9ec73517fdccefe2da79b137fffd4b
junsheng <leejunsheng7@gmail.com>
2023-12-27 18:32:23 +0800

    Completed skills and qualification section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
+import About from "./components/about/About";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Qualification from "./components/qualification/Qualification";
+import Skills from "./components/skills/Skills";
 
 export default function App() {
   return (
@@ -8,6 +11,9 @@ export default function App() {
       <Header />
       <main className="main">
         <Home />
+        <About />
+        <Skills />
+        <Qualification />
       </main>
     </>
   );
