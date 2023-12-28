fb1dcb01af0f23e1b2bd63d7a1075774573dc99f
junsheng <leejunsheng7@gmail.com>
2023-12-28 18:08:52 +0800

    Edited portfolio to projects

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -4,7 +4,7 @@ import "./work.css";
 export default function Work() {
   return (
     <section className="work section" id="portfolio">
-      <h2 className="section__title">Portfolio</h2>
+      <h2 className="section__title">Projects</h2>
       <span className="section__subtitle">Most recent works</span>
 
       <Works />
